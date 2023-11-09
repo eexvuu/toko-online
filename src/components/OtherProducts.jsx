1c1ad06b30ce905530426c2214840a01ce4a692d
usamah abdul matin <eexvuu@gmail.com>
2023-11-09 20:34:59 +0700

    first commit

---
+++ src/components/OtherProducts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function OtherProducts() {
+  return <div>OtherProducts</div>;
+}
+
+export default OtherProducts;
---
+++ src/components/PopularByCategory.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PopularByCategory() {
+  return <div>PopularByCategory</div>;
+}
+
+export default PopularByCategory;
